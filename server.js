1fe534328336fd02702311b12c3a3bcba32d35bb
xxroltsxx <xxroltsxx@hotmail.nl>
2015-06-08 11:27:25 +0200

    Basic setup

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+// Dependencies
 var bodyParser = require('body-parser');
 var express = require('express');
 var swig = require('swig');
 
+// Knex login to database
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : 'Yne4RGMygSu4PvSE',
+    database : 'iojspanel'
+  }
+});
+
+// Ask Express to kindly start itself
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +36,7 @@ app.post('/test', function(req, res) {
 	res.send(req.body);
 });
 
+//Express details
 var server = app.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
